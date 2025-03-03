545bb55a8c3f2ff2618021a06a0119db437e1f9a
Bhumi-Tiwari04 <bhumi520054@gmail.com>
2025-03-03 23:16:23 +0530

    Updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,7 @@ const {validationResult}=require('express-validator');
 
 
 
-module.exports.registerUser=async(req,resizeBy,next)=>{
+module.exports.registerUser=async(req,res,next)=>{
     const errors=validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()})
@@ -14,8 +14,8 @@ module.exports.registerUser=async(req,resizeBy,next)=>{
     const hashPassword=await userModel.hashPassword(password);
 
     const user=await userService.createUser({
-        firstname,
-        lastname,
+        firstname:fullname.firstname,
+        lastname:fullname.lastname,
         email,
         password:hashPassword
     });
